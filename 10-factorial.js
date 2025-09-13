8db842d94d6f7c6413fa42a307d25900029978c0
noura_abidar <nourssin0125@gmail.com>
2025-09-13 16:17:10 -0700

    add 10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const n = parseInt(process.argv[2]);
+
+function factorial(x) {
+    if (isNaN(x) || x <= 1) return 1;
+    return x * factorial(x - 1);
+}
+
+console.log(factorial(n));
