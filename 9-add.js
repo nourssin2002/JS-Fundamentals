10678c7075e23e79915be52f316dee98897fcdae
noura_abidar <nourssin0125@gmail.com>
2025-09-13 16:06:25 -0700

    Add 9-add.js script

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+function add(a, b) {
+    return a + b;
+}
+
+const num1 = parseInt(args[0]);
+const num2 = parseInt(args[1]);
+
+console.log(add(num1, num2));
